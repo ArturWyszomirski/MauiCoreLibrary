1059df58bf877c6e6f410d8f28de527d297b6c1d
Artur <a.wyszomirski@etongroup.eu>
2023-04-28 11:35:02 +0200

    Enums converted to file-scoped namespace.

--- Enums.cs
+++ Enums.cs
@@ -1,13 +1,12 @@
-﻿namespace MauiCoreLibrary
+﻿namespace MauiCoreLibrary;
+
+[FlagsAttribute]
+public enum EXECUTION_STATE : uint
 {
-    [FlagsAttribute]
-    public enum EXECUTION_STATE : uint
-    {
-        ES_AWAYMODE_REQUIRED = 0x00000040,
-        ES_CONTINUOUS = 0x80000000,
-        ES_DISPLAY_REQUIRED = 0x00000002,
-        ES_SYSTEM_REQUIRED = 0x00000001
-        // Legacy flag, should not be used.
-        // ES_USER_PRESENT = 0x00000004
-    }
+    ES_AWAYMODE_REQUIRED = 0x00000040,
+    ES_CONTINUOUS = 0x80000000,
+    ES_DISPLAY_REQUIRED = 0x00000002,
+    ES_SYSTEM_REQUIRED = 0x00000001
+    // Legacy flag, should not be used.
+    // ES_USER_PRESENT = 0x00000004
 }
