e2a59d09b0974108500b794e435f313d5b1d3712
Artur Wyszomirski <artur.d.w@gmail.com>
2023-06-15 23:47:14 +0200

    HttpClientService extracted to MauiCoreLibrary.

---
+++ Services/HttpClientService.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace MauiCoreLibrary.Services;
+
+public class HttpClientService
+{
+    public static async Task<Dictionary<string, object>> PostJsonAsync(string uri, string json)
+    {
+        using HttpClient client = new();
+        StringContent content = new(json, Encoding.UTF8, "application/json");
+        using HttpResponseMessage response = await client.PostAsync(uri, content);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonResponse);
+        }
+        else
+            throw new Exception($"Request unsuccessful!\n\nRequest message:\n{response.RequestMessage}\n\nStatus code:\n{response.StatusCode}\n\nReason:\n{response.ReasonPhrase}");
+    }
+}
