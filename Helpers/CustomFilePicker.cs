be7a9dd79927b3402301911053fb6d6df58f6025
Artur Wyszomirski <artur.d.w@gmail.com>
2023-06-15 19:38:52 +0200

    CSV file picker added to helpers.

---
+++ Helpers/CustomFilePicker.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiCoreLibrary.Helpers;
+
+public class CustomFilePicker
+{
+    public static async Task<FileResult> PickCsvFile()
+    {
+        return await FilePicker.PickAsync(new PickOptions
+        {
+            PickerTitle = "Select CSV File",
+            FileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+            {
+                { DevicePlatform.Android, new[] { "application/csv", "text/csv" } },
+                { DevicePlatform.iOS, new[] { "com.microsoft.csv" } },
+                { DevicePlatform.WinUI, new[] { ".csv" } }
+            })
+        });
+    }
+}
