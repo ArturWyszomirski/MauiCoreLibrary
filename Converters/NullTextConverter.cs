e15052464860798f8df1e60c6b6938528e48e1a7
Artur Wyszomirski <artur.d.w@gmail.com>
2023-06-24 17:58:23 +0200

    NullTextConverter added.

---
+++ Converters/NullTextConverter.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiCoreLibrary.Converters;
+
+public class NullTextConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (string.IsNullOrEmpty(value as string))
+            return parameter;
+
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
