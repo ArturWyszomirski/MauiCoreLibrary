964d3b1000d7274bf6ec5b889458937f9c23ce16
Artur Wyszomirski <artur.d.w@gmail.com>
2023-06-27 15:38:32 +0200

    Shell.Current null check added to AlertService.

--- Services/AlertService.cs
+++ Services/AlertService.cs
@@ -4,11 +4,24 @@ public class AlertService : IAlertService
 {
     public async Task DisplayAlertAsync(string title, string message, string confirmation)
     {
-        await Shell.Current.DisplayAlert(title, message, confirmation);
+        if (Shell.Current is not null)
+            await Shell.Current.DisplayAlert(title, message, confirmation);
+        else
+        {
+            string errorMessage = $"{nameof(Shell.Current)} is null."; // try-catch won't handle exception from UI thread
+            Debug.WriteLine(errorMessage);
+        }
     }
 
     public async Task<bool> DisplayAlertAsync(string title, string message, string confirmation, string cancelation)
     {
-        return await Shell.Current.DisplayAlert(title, message, confirmation, cancelation);
+        if (Shell.Current is not null)
+            return await Shell.Current.DisplayAlert(title, message, confirmation, cancelation);
+        else
+        {
+            string errorMessage = $"{nameof(Shell.Current)} is null."; // try-catch won't handle exception from UI thread
+            Debug.WriteLine(errorMessage);
+            throw new Exception(errorMessage);
+        }
     }
 }
