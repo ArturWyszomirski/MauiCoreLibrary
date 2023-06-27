d43e35c1ce3a6d770d15e4b81cd4bc690f2bd605
Artur Wyszomirski <artur.d.w@gmail.com>
2023-06-27 15:21:14 +0200

    TimeStamp added to Helpers.

---
+++ Helpers/TimeStamp.cs
@@ -0,0 +1,6 @@
+﻿namespace MauiCoreLibrary.Helpers;
+
+public class TimeStamp
+{
+    public static string TimeStampDashed => $"{DateTime.Now.Year}_{DateTime.Now.Month}_{DateTime.Now.Day}_{DateTime.Now.Hour}_{DateTime.Now.Minute}_{DateTime.Now.Second}";
+}
