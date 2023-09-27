7a0668fb80eebda129881738e0aa3107a1ce72f9
Artur Wyszomirski <artur.d.w@gmail.com>
2023-09-27 12:29:24 +0200

    Add Onnx in Helpers and move there LoadOnnx method.

---
+++ Helpers/Onnx.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.ML.OnnxRuntime.Tensors;
+using Microsoft.ML.OnnxRuntime;
+
+namespace MauiCoreLibrary.Helpers;
+
+public class Onnx
+{
+    /// <summary>
+    /// ONNX file should be put in the ../Resources/Raw directory.
+    /// </summary>
+    /// <returns></returns>
+    public static async Task<InferenceSession> LoadOnnxAsync(string fileName)
+    {
+        using Stream stream = await FileSystem.OpenAppPackageFileAsync(fileName);
+
+        MemoryStream memoryStream = new(); // Need to be done with MemoryStream as Stream.Length and Stream.Position are not supported under Android. 
+        stream.CopyTo(memoryStream);
+
+        int streamLength = memoryStream.GetBuffer().Length;
+        byte[] buffer = new byte[streamLength];
+        memoryStream.Position = 0;
+        memoryStream.Read(buffer, 0, buffer.Length);
+
+        InferenceSession session = new(buffer);
+
+        return session;
+    }
+}
