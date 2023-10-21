67a38873cf208fdd763abe40180de6c9a422464b
Artur Wyszomirski <artur.d.w@gmail.com>
2023-10-21 16:06:03 +0200

    Add stream to byte[] converter.

---
+++ Helpers/Converters.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiCoreLibrary.Helpers;
+public class Converters
+{
+    public static byte[] StreamToBytes(Stream stream)
+    {
+        using MemoryStream memoryStream = new();
+        stream.CopyTo(memoryStream);
+        return memoryStream.ToArray();
+    }
+}
