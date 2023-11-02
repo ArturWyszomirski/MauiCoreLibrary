68417d0cfc748900ff94e919b30b455e975a5bbe
Artur Wyszomirski <artur.d.w@gmail.com>
2023-11-02 19:41:27 +0100

    Update full name property.

--- Models/PersonalDataModel.cs
+++ Models/PersonalDataModel.cs
@@ -27,7 +27,19 @@ public partial class PersonalDataModel : ObservableRecipient, IPersonalDataModel
     [NotifyPropertyChangedRecipients]
     private string _phoneNumber;
 
-    public string FullName { get => $"{FirstName} {LastName}"; }
+    public string FullName {
+        get
+        {
+            if (!string.IsNullOrEmpty(FirstName) && !string.IsNullOrEmpty(LastName))
+                return $"{FirstName} {LastName}";
+            else if (!string.IsNullOrEmpty(FirstName) && string.IsNullOrEmpty(LastName))
+                return $"{FirstName}";
+            else if (string.IsNullOrEmpty(FirstName) && !string.IsNullOrEmpty(LastName))
+                return $"{LastName}";
+            else
+                return null;
+        }
+    }
 
     public virtual void ClearData()
     {
