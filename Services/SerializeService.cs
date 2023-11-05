4762e53e9a918ba6380853ae4306abc102b48812
Artur Wyszomirski <artur.d.w@gmail.com>
2023-11-05 20:16:02 +0100

    Update with file scoped namespace.

--- Services/SerializeService.cs
+++ Services/SerializeService.cs
@@ -1,51 +1,50 @@
-﻿namespace MauiCoreLibrary.Services
+﻿namespace MauiCoreLibrary.Services;
+
+public class SerializeService : ISerializeService
 {
-    public class SerializeService : ISerializeService
+    private readonly IAlertService _alert;
+    private readonly IFileLogService _fileLog;
+
+    public SerializeService(IAlertService alert, IFileLogService fileLog)
     {
-        private readonly IAlertService _alert;
-        private readonly IFileLogService _fileLog;
+        _alert = alert;
+        _fileLog = fileLog;
+    }
 
-        public SerializeService(IAlertService alert, IFileLogService fileLog)
+    public async Task<T> DeserializeObjectFromJsonFileAsync<T>(string directory, string fileName)
+    {
+        string filePath = Path.Combine(directory, fileName);
+        try
         {
-            _alert = alert;
-            _fileLog = fileLog;
+            using FileStream fileStream = File.OpenRead(filePath);
+            DataContractJsonSerializer serializer = new(typeof(T));
+            return (T)serializer.ReadObject(fileStream);
         }
-
-        public async Task<T> DeserializeObjectFromJsonFileAsync<T>(string directory, string fileName)
+        catch (Exception ex)
         {
-            string filePath = Path.Combine(directory, fileName);
-            try
-            {
-                using FileStream fileStream = File.OpenRead(filePath);
-                DataContractJsonSerializer serializer = new(typeof(T));
-                return (T)serializer.ReadObject(fileStream);
-            }
-            catch (Exception ex)
-            {
-                _fileLog.AppendLine(ex.ToString());
-                await _alert?.DisplayAlertAsync("Error", $"JSON serialization error: {ex.Message}", "Ok");
-                return default;
-            }
+            _fileLog.AppendLine(ex.ToString());
+            await _alert?.DisplayAlertAsync("Error", $"JSON serialization error: {ex.Message}", "Ok");
+            return default;
         }
+    }
 
-        public async Task<bool> SerializeObjectToJsonFileAsync<T>(T obj, string directory, string fileName)
+    public async Task<bool> SerializeObjectToJsonFileAsync<T>(T obj, string directory, string fileName)
+    {
+        string filePath = Path.Combine(directory, fileName);
+        try
         {
-            string filePath = Path.Combine(directory, fileName);
-            try
-            {
-                using MemoryStream memoryStream = new();
-                DataContractJsonSerializer serializer = new(typeof(T));
-                serializer.WriteObject(memoryStream, obj);
-                Directory.CreateDirectory(directory);
-                File.WriteAllText(filePath, Encoding.UTF8.GetString(memoryStream.ToArray()));
-                return true;
-            }
-            catch (Exception ex)
-            {
-                _fileLog.AppendLine(ex.ToString());
-                await _alert?.DisplayAlertAsync("Error", $"JSON serialization error: {ex.Message}", "Ok");
-                return false;
-            }
+            using MemoryStream memoryStream = new();
+            DataContractJsonSerializer serializer = new(typeof(T));
+            serializer.WriteObject(memoryStream, obj);
+            Directory.CreateDirectory(directory);
+            File.WriteAllText(filePath, Encoding.UTF8.GetString(memoryStream.ToArray()));
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _fileLog.AppendLine(ex.ToString());
+            await _alert?.DisplayAlertAsync("Error", $"JSON serialization error: {ex.Message}", "Ok");
+            return false;
         }
     }
 }
