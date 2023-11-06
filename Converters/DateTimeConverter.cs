f40867e4243414b7e40109990229b87219ffe887
Artur Wyszomirski <artur.d.w@gmail.com>
2023-11-06 16:32:43 +0100

    Add DateTimeConverter.

---
+++ Converters/DateTimeConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiCoreLibrary.Converters;
+
+public class DateTimeConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is DateTime dateTime)
+        {
+            if (parameter is string format)
+                return dateTime.ToString(format);
+            else
+                throw new ArgumentException($"{nameof(parameter)} must be a string.");
+        }
+        else
+            throw new ArgumentException($"{nameof(value)} must be a DateTime.");
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
