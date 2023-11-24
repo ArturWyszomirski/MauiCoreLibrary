6e6e88cdc974c57192e6032404c036d5509b27f7
Artur Wyszomirski <artur.d.w@gmail.com>
2023-11-24 18:03:50 +0100

    Update app directories for Windows.

--- Models/AppSettingsModel.cs
+++ Models/AppSettingsModel.cs
@@ -1,14 +1,12 @@
-﻿using Microsoft.VisualBasic.FileIO;
-
-namespace MauiCoreLibrary.Models;
+﻿namespace MauiCoreLibrary.Models;
 
 public class AppSettingsModel : ModelBase, IAppSettingsModel
 {
     public AppSettingsModel()
     {
 #if WINDOWS
-        AppDataDirectory = SpecialDirectories.CurrentUserApplicationData;
-        AppDocumentsDirectory = SpecialDirectories.MyDocuments;
+        AppDataDirectory = FileSystem.Current.AppDataDirectory;
+        AppDocumentsDirectory = Microsoft.VisualBasic.FileIO.SpecialDirectories.MyDocuments;
 #elif ANDROID
         AppDataDirectory = (string)Android.App.Application.Context.GetExternalFilesDir((string)Android.OS.Environment.DataDirectory);
         AppDocumentsDirectory = (string)Android.App.Application.Context.GetExternalFilesDir((string)Android.OS.Environment.DirectoryDocuments);
